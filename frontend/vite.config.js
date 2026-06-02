741710ea17e89b2ed646309871ad8d59d2e51be2
joseP0029 <josepinzon0029@gmail.com>
2026-06-02 21:43:42 +0000

    configuracion de vite y app

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      "super-duper-waddle-r4rwq5pjqq9gfx95g-5173.app.github.dev"
+    ],
+    proxy: {
+      // Intercepta las llamadas a /api y las manda al servidor Express
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+        // Opcional: útil si tienes problemas de WebSocket o rutas complejas
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      }
+    }
+  }
 })
